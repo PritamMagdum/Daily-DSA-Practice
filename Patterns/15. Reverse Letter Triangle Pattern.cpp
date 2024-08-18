49666bd9e2ba1198d4d40e83d973386c268486cd
Pritam <pritammagdum24@gmail.com>
2024-08-18 22:18:55 +0530

    Solved 15. Reverse Letter Triangle Pattern

---
+++ Patterns/15. Reverse Letter Triangle Pattern.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+ABCDE
+ABCD
+ABC
+AB
+A
+
+*/
+
+void pattern15(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i); ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cout << "Enter test cases count" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern15(n);
+    }
+}
