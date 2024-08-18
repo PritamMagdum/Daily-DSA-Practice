e0239f90beeaf6cfdd20e2bdf7a7821dbc59594d
Pritam <pritammagdum24@gmail.com>
2024-08-18 22:18:28 +0530

    Solved 14. Increasing Letter Triangle Pattern

---
+++ Patterns/14. Increasing Letter Triangle Pattern.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
+
+void pattern14(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cout << "Enter test cases count" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern14(n);
+    }
+}
