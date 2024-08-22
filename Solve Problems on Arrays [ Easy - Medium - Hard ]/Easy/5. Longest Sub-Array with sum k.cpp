829177d607cf71409edeebac0afe78771ff1def9
Pritam <pritammagdum24@gmail.com>
2024-08-22 23:10:09 +0530

    Solved 5. Longest sub-array with sum k

---
+++ Solve Problems on Arrays [ Easy - Medium - Hard ]/Easy/5. Longest Sub-Array with sum k.cpp
@@ -0,0 +1,25 @@
+int lenOfLongSubarr(int arr[], int n, int k)
+{
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+        long long rem = sum - k;
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if (preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
