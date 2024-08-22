b8219e1927114d838fdf64bb076594af5058c63a
Pritam <pritammagdum24@gmail.com>
2024-08-22 23:08:23 +0530

    Solved 1. Largest element in an array

---
+++ Solve Problems on Arrays [ Easy - Medium - Hard ]/Easy/1. Largest element in an Array.cpp
@@ -0,0 +1,12 @@
+// Largest element in the array
+
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    int largest = arr[0];
+    for(int i = 0;i<n;i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
