5f12135839418fe9085d8f0e3e417db5b5dcd74d
Pritam <pritammagdum24@gmail.com>
2024-08-25 23:02:33 +0530

    Solved Count Inversions

---
+++ Solve Problems on Arrays [ Easy - Medium - Hard ]/Hard/Count Inversions.cpp
@@ -0,0 +1,66 @@
+long long merge(long long arr[], int l, int mid, int r)
+{
+    long long inv = 0;
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    long a[n1];
+    long b[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        a[i] = arr[l + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        b[i] = arr[mid + i + 1];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+        if (a[i] <= b[j])
+        {
+            arr[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            arr[k] = b[j];
+            inv += n1 - i;
+            k++;
+            j++;
+        }
+    }
+    while (i < n1)
+    {
+        arr[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j < n2)
+    {
+        arr[k] = b[j];
+        k++;
+        j++;
+    }
+    return inv;
+}
+
+long long mergeSort(long long arr[], int l, int r)
+{
+    long long inv = 0;
+    if (l < r)
+    {
+        int mid = (l + r) / 2;
+        inv += mergeSort(arr, l, mid);
+        inv += mergeSort(arr, mid + 1, r);
+        inv += merge(arr, l, mid, r);
+    }
+    return inv;
+}
+long long int inversionCount(long long arr[], int n)
+{
+    mergeSort(arr, 0, n - 1);
+}
