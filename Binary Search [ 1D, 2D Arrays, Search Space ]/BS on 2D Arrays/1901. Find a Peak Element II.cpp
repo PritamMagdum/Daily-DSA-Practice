58eb8ff4f9571cfc4845a29619acf398ddf74bcb
Pritam <pritammagdum24@gmail.com>
2024-09-01 22:55:33 +0530

    Solved 1901. Find a Peak Element ll

---
+++ Binary Search [ 1D, 2D Arrays, Search Space ]/BS on 2D Arrays/1901. Find a Peak Element II.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+private:
+    int findMaxIndex(vector<vector<int>> &g, int n, int m, int col)
+    {
+        int maxValue = -1;
+        int index = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (g[i][col] > maxValue)
+            {
+                maxValue = g[i][col];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+public:
+    vector<int> findPeakGrid(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0, high = m - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int maxRowIndex = findMaxIndex(mat, n, m, mid);
+            int left = mid - 1 >= 0 ? mat[maxRowIndex][mid - 1] : -1;
+            int right = mid + 1 < m ? mat[maxRowIndex][mid + 1] : -1;
+            if (mat[maxRowIndex][mid] > left && mat[maxRowIndex][mid] > right)
+            {
+                return {maxRowIndex, mid};
+            }
+            else if (mat[maxRowIndex][mid] < left)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return {-1, -1};
+    }
+};
