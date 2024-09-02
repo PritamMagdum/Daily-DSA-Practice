7ba3fbd9fff004aedb95b8e9f71dba2229937ff9
Pritam <pritammagdum24@gmail.com>
2024-09-02 23:46:28 +0530

    Solved 242. Valid Anagram

---
+++ Strings [ Basic and Medium ]/Basic and Easy String Problems/242. Valid Anagram.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        vector<int> count(26);
+
+        for (char &ch : s)
+        {
+            count[ch - 'a']++;
+        }
+
+        for (char &ch : t)
+        {
+            count[ch - 'a']--;
+        }
+
+        bool allZeros = all_of(begin(count), end(count), [](int element)
+                               { return element == 0; });
+        return allZeros;
+    }
+};
