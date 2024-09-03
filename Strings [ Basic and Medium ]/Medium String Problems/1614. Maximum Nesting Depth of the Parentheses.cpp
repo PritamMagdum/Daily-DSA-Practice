608eef1b61d7060ddcd05f8e0a42e1158b4f256e
Pritam <pritammagdum24@gmail.com>
2024-09-03 21:39:30 +0530

    Solved 1614. Maximum Nesting Depth of the Parantheses

---
+++ Strings [ Basic and Medium ]/Medium String Problems/1614. Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxDepth(string s)
+    {
+        int cnt = 0;
+        int ans = 0;
+        for (auto ch : s)
+        {
+            if (ch == '(')
+                cnt++;
+            else if (ch == ')')
+                cnt--;
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
