bb39db6394b3628e4b9bea8ace120188ce7faf7a
Pritam <pritammagdum24@gmail.com>
2024-09-06 00:45:28 +0530

    Solved Search in a Linked List

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Learn 1D LinkedList/Search in a Linked List.cpp
@@ -0,0 +1,11 @@
+int searchInLinkedList(Node<int> *head, int k)
+{
+    Node<int> *temp = head;
+    while (temp)
+    {
+        if (temp->data == k)
+            return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
