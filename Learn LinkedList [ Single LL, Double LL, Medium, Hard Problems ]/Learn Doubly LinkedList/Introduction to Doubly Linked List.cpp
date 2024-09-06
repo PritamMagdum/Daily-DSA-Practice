d406f8fc644fbbb407e45011a9ca3e7b66f39b6c
Pritam <pritammagdum24@gmail.com>
2024-09-06 19:48:43 +0530

    Solved Introduction to Doubly Linked List ( Array to Doubly Linked List )

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Learn Doubly LinkedList/Introduction to Doubly Linked List.cpp
@@ -0,0 +1,30 @@
+// Example - 1
+Node *constructDLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+// Example - 2
+Node *constructDLL(vector<int> &arr)
+{
+    if (arr.size() == 0)
+        return nullptr;
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        temp->prev = mover;
+        mover = mover->next;
+    }
+    return head;
+}
