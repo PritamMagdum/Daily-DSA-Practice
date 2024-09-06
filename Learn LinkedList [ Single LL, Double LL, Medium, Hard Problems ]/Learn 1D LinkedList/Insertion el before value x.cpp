8dc6b9b856a5014030b840bbe8e956de1ac51290
Pritam <pritammagdum24@gmail.com>
2024-09-06 13:28:50 +0530

    Solved Insertion El Before Value x

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Learn 1D LinkedList/Insertion el before value x.cpp
@@ -0,0 +1,27 @@
+class solution
+{
+public:
+    Node *insertBeforeVal(Node *head, int el, int val)
+    {
+        if (head == NULL)
+        {
+            return NULL;
+        }
+        if (head->data == val)
+        {
+            return new Node(el, head);
+        }
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            if (temp->next->data == val)
+            {
+                Node *x = new Node(el, temp->next);
+                temp->next = x;
+                break;
+            }
+            temp = temp->next;
+        }
+        return head;
+    }
+}
