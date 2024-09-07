617302f09297d40c04ffc7f7f98b6af1e1db5ece
Pritam <pritammagdum24@gmail.com>
2024-09-07 11:42:34 +0530

    Solved 876. Middle of the Linked List

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Medium Problems of LL/876. Middle of the Linked List.cpp
@@ -0,0 +1,18 @@
+// Tortoise and Hare Algorithm
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slowPointer = head;
+        ListNode *fastPointer = head;
+
+        while (fastPointer != nullptr && fastPointer->next != nullptr)
+        {
+            slowPointer = slowPointer->next;
+            fastPointer = fastPointer->next->next;
+        }
+
+        return slowPointer;
+    }
+};
