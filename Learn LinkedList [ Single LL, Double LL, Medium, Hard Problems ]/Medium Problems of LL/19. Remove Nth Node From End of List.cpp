4136f26330f5eb99fe33b5040b9f81b04472cc39
Pritam <pritammagdum24@gmail.com>
2024-09-08 12:10:14 +0530

    Solved 19. Remove Nth Node From End of Linked List

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Medium Problems of LL/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        for(int i=0;i<n;i++) fast = fast->next;
+        if(!fast){
+            ListNode* delNode = head;
+            head = head->next;
+            delete delNode;
+            return head;
+        }
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
+        return head;
+    }
+};
