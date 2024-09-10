bdf46599f1864c81cfc48119fb2034d131c1d941
Pritam <pritammagdum24@gmail.com>
2024-09-10 23:38:20 +0530

    Solved 138. Copy List with Random Pointer

---
+++ Learn LinkedList [ Single LL, Double LL, Medium, Hard Problems ]/Hard Problems of LL/138. Copy List with Random Pointer.cpp
@@ -0,0 +1,63 @@
+class Solution
+{
+public:
+    Node *insertCopyInBetween(Node *head)
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            Node *nextElement = temp->next;
+            Node *copy = new Node(temp->val);
+
+            copy->next = nextElement;
+            temp->next = copy;
+
+            temp = nextElement;
+        }
+        return head;
+    }
+
+    Node *connectRandomPointers(Node *head)
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            Node *copyNode = temp->next;
+
+            if (temp->random)
+            {
+                copyNode->random = temp->random->next;
+            }
+            else
+            {
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+        return head;
+    }
+
+    Node *getDeepCopyList(Node *head)
+    {
+        Node *temp = head;
+        Node *dummyNode = new Node(-1);
+        Node *res = dummyNode;
+
+        while (temp != NULL)
+        {
+            res->next = temp->next;
+            res = res->next;
+
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+
+    Node *copyRandomList(Node *head)
+    {
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
+    }
+};
