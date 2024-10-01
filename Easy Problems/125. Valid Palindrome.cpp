295babf627b9e57015236f3bdde22b57243214f7
Pritam <pritammagdum24@gmail.com>
2024-10-01 23:06:21 +0530

    Solved 125. Valid Palindrome

---
+++ Easy Problems/125. Valid Palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string pal = "";
+        for (char i : s) {
+            i = tolower(i);
+            if (isalpha(i) || isdigit(i)) pal.push_back(i);
+        }
+
+        int n = pal.size(), left = 0, right = n - 1;
+        if (n == 0 || n == 1) return true;
+        while (left <= right) {
+            if (pal[left] == pal[right]) {
+                left++;
+                right--;
+            } else return false;
+        }
+        return true;
+    }
+};
