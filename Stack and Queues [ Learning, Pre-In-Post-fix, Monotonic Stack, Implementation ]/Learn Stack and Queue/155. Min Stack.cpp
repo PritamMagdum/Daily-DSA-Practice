b5ef59d4529412389ac503c0238b234ad8784860
Pritam <pritammagdum24@gmail.com>
2024-10-05 19:19:46 +0530

    Solved 155. Min Stack

---
+++ Stack and Queues [ Learning, Pre-In-Post-fix, Monotonic Stack, Implementation ]/Learn Stack and Queue/155. Min Stack.cpp
@@ -0,0 +1,60 @@
+class MinStack
+{
+public:
+    stack<long long> st;
+    long long min = 0;
+
+    MinStack() {}
+
+    void push(int val)
+    {
+        if (st.empty())
+        {
+            min = val;
+            st.push(val);
+        }
+        else
+        {
+            if (val > min)
+                st.push(val);
+            else
+            {
+                st.push(2LL * val - min);
+                min = val;
+            }
+        }
+    }
+
+    void pop()
+    {
+        if (st.empty())
+            return;
+        long long x = st.top();
+        st.pop();
+        if (x < min)
+        {
+            min = 2LL * min - x;
+        }
+    }
+
+    int top()
+    {
+        if (st.empty())
+            return -1;
+        long long x = st.top();
+        if (min < x)
+            return x;
+        return min;
+    }
+
+    int getMin() { return (int)min; }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
