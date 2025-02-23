776f7e271a04e9114e84c61936e4143c1919be9a
Pritam <pritammagdum24@gmail.com>
2025-02-23 22:12:00 +0530

    Solved 145. Binary Tree Iterative Postorder Traversal

---
+++ Binary Trees [ Traversals, Medium and Hard Problems ]/Traversals/145. Binary Tree Iterative Postorder Traversal using Two Stack.cpp
@@ -0,0 +1,37 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> postorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    stack<TreeNode*> st1,st2;
+    st1.push(root);
+
+    while(!st1.empty()){
+        root = st1.top();
+        st1.pop();
+        st2.push(root);
+
+        if(root->left != nullptr){
+            st1.push(root->left);
+        }
+        if(root->right != nullptr){
+            st1.push(root->right);
+        } 
+    }
+
+    while(!st2.empty()){
+        ans.push_back(st2.top()->data);
+        st2.pop();
+    }
+    return ans;
+}
